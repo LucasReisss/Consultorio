d08a239765bd3193e254284916e3a725f3886c09
LucasReisss <lucasbarroreis@gmail.com>
2020-03-26 20:26:23 -0300

    Atualização

--- Consultorio/src/com/consultorio/controller/LoginController.java
+++ Consultorio/src/com/consultorio/controller/LoginController.java
@@ -1,16 +1,11 @@
 package com.consultorio.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
-import javax.swing.JOptionPane;
 
 import com.consultorio.application.Util;
 import com.consultorio.factory.JPAFactory;
@@ -24,52 +19,23 @@ public class LoginController extends Controller<Usuario> {
 
 	private Usuario usuario;
 	UsuarioController usCon = new UsuarioController();
-	
-	
-	public String login() {
-		
-		
-		
-//		List<Usuario> usuarios = new ArrayList<Usuario>();
-//		usuarios = (List<Usuario>) getUsuario(getEntity().getEmail(), getEntity().getSenha());
-//
-//		for (Usuario usuarioDaLista : usuarios) {
-//			if (getUsuario().getEmail().equals(usuarioDaLista.getEmail())) {
-//				getUsuario().setSenha(Util.hashSHA256(getUsuario().getSenha()));
-//				if (getUsuario().getSenha().equals(usuarioDaLista.getSenha())) {
-//					System.out.println("igual");
-//
-//					if (usuario != null) {
-//						// armazenando um usuario na sessao
-//						return "paciente.xhtml?faces-redirect=true";
-//
-//					}
-//
-//				}
-//			}
-//			
-//	}
-//		Util.addMessageError("Usuário ou senha Inválido.");
-//		return null;
-	
 
+	public String logar() {
+		FacesContext context = FacesContext.getCurrentInstance();
 		String senhacrip = Util.hashSHA256(getEntity().getSenha());
-		usuario = getUsuario(getEntity().getEmail(), senhacrip);
-		
-		
-	if(usuario != null) {
-		Util.addMessageInfo("Login realizado com Sucesso");
+		System.out.println("Email: " + entity.getEmail() + " senha: " + senhacrip);
+		if (usCon.logar(entity.getEmail(), senhacrip) == true) {
 			return "paciente.xhtml?faces-redirect=true";
 		}
-			Util.addMessageError("Usuário ou senha Inválido");
-			return null;
+		Util.addMessageError("Email ou senha incorretos");
+		return " . ";
 	}
-
 
 	public Usuario getUsuario(String email, String senha) {
 		try {
 			EntityManager em = JPAFactory.getEntityManager();
-			Query query = em.createQuery("SELECT m " + "FROM Usuario m " + "where m.email = :email and m.senha = :senha");
+			Query query = em
+					.createQuery("SELECT m " + "FROM Usuario m " + "where m.email = :email and m.senha = :senha");
 			query.setParameter("email", "email");
 			query.setParameter("senha", "senha");
 			return entity;
@@ -77,39 +43,20 @@ public class LoginController extends Controller<Usuario> {
 			return null;
 		}
 	}
-	
-
-
-	
 
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
 
-
-
 	public Usuario getUsuario() {
 		return usuario;
 	}
-
 
 	@Override
 	public Usuario getEntity() {
 		if (entity == null)
 			entity = new Usuario();
 		return entity;
-	}
-	
-	
-	public String logar() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		String senhacrip = Util.hashSHA256(getEntity().getSenha());
-		System.out.println("Email: " + entity.getEmail() + " senha: " + senhacrip);
-		if(usCon.logar(entity.getEmail(), senhacrip) == true) {
-			return "paciente.xhtml?faces-redirect=true";
-		}
-		context.addMessage(null, new FacesMessage("Email ou senha incorretos"));
-		return " . ";
 	}
 
 }
